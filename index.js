178d9f7a2996237c30acf2f3fe8ce08b838a26ca
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-19 14:31:03 -0500

    Task 3

--- index.js
+++ index.js
@@ -109,6 +109,14 @@ drive(distance){
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes) {
+    this.name = attributes.name, 
+    this.age = attributes.age, 
+    this.location = attributes.location
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`
+  }
 
 }
 
