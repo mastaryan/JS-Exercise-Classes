e1c1f5f97179d7b5835a994b4245197fc0574b30
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-19 14:53:40 -0500

    Task 6

--- index.js
+++ index.js
@@ -196,8 +196,18 @@ sprintChallenge(subject){
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+  constructor(attributes) {
+    super(attributes);
+    this.gradClassName = attributes.gradClassName,
+    this.favInstructor = attributes.favInstructor
+  }
+standUp(slack-channel) {
+  return `{this.name} announces to ${slack-channel}, @channel standy times!`
+}
+debugsCode(student, subject) {
+  return `${this.name} debugs ${student.name}'s code on ${subject}`
+}
 }
 
 /*
